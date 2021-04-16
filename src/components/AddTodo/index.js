6fa5c2d3c73af77ed3b0b0b69609b32813b8c651
chris westerduin <chriswesterduin@outlook.com>
2021-04-16 16:26:20 +0100

    add dispatch to AddTodo

--- src/components/AddTodo/index.js
+++ src/components/AddTodo/index.js
@@ -1,11 +1,49 @@
 import React, { useState } from 'react'
+import { useDispatch } from 'react-redux';
+import { addTodo } from '../../actions';
 
 const AddTodo = () => {
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
+  const dispatch = useDispatch()
+
+  function submitTodo(e) {
+    e.preventDefault();
+    dispatch(addTodo({title, body}))
+    setTitle('')
+    setBody('')
+  }
+
   return (
     <tr>
-      <td><textarea style={{resize: "vertical", width: '100%',  minHeight: '40px'}} id="title" name="title" form="form1"></textarea></td>
-      <td><textarea style={{resize: "vertical", width: '100%', minHeight: '40px'}} id="body" name="body" form="form1"></textarea></td>
-      <td><input type="submit" value="submit" form="form1" /></td>
+      <td>
+        <textarea
+          style={{ resize: "vertical", width: "100%", minHeight: "40px" }}
+          id="title"
+          name="title"
+          form="form1"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        ></textarea>
+      </td>
+      <td>
+        <textarea
+          style={{ resize: "vertical", width: "100%", minHeight: "40px" }}
+          id="body"
+          name="body"
+          form="form1"
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+      </td>
+      <td>
+        <input
+          onClick={(e) => submitTodo(e)}
+          type="submit"
+          value="submit"
+          form="form1"
+        />
+      </td>
     </tr>
   );
 };
