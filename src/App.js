86f614f2c2d76755bc605357aa87c9498bb49f39
chris westerduin <chriswesterduin@outlook.com>
2021-04-17 00:17:21 +0100

    set initial state from local storage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Switch, Route } from 'react-router-dom';
 import { Home, Todo, TodoSolo } from './pages'
 import { Layout } from './layout'
+import { useSelector, useDispatch } from 'react-redux'
+import { loadState } from "./actions";
+
 const App = () => {
+    const dispatch = useDispatch()
+
+    const appState = useSelector((state) => state);
+
+    useEffect(() => {
+        const storeState = localStorage.getItem('state')
+        console.log(storeState.length)
+        if (storeState.length > 2) {
+        dispatch(loadState(JSON.parse(storeState)))
+        }
+        else {}
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem('state', JSON.stringify(appState))
+    }, [appState]);
+
   return (
     <Layout>
       <Switch>
