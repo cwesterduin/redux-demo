aee5efcc6ade26909e2a9f03e7bce2963087a14d
chris westerduin <chriswesterduin@outlook.com>
2021-04-16 15:24:07 +0100

    initial reducer and action setup

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,1 @@
-// write redux actions here
-// e.g.
-// export const addUser = newUser => ({ type: 'ADD_USER', payload: newUser })
+export const addTodo = content => ({ type: 'ADD_TODO', payload: content })
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,25 +1,10 @@
-// combine multiple reducers
+const todoState = (state = [{title: 'create todos', body: 'see above'}], action) => {
+    switch (action.type) {
+      case 'ADD_TODO':
+        return state.concat([action.payload])
+      default:
+        return state
+    }
+  }
 
-// import { combineReducers } from 'redux'
-// import reducer1 from './reducer1'
-// import reducer2 from './reducer2'
-
-// export default combineReducers({
-//     reducer1,
-//     reducer2
-// })
-
-// OR write simple reducer
-
-// const counter = (state = 0, action) => {
-//     switch (action.type) {
-//       case 'INCREMENT':
-//         return state + 1
-//       case 'DECREMENT':
-//         return state - 1
-//       default:
-//         return state
-//     }
-//   }
-
-//   export default counter;
+  export default todoState;
