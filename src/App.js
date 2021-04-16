ed34f1be427a174ad831ba9495d80c516c5a38b6
chris westerduin <chriswesterduin@outlook.com>
2021-04-17 00:36:51 +0100

    move local state storage logic to custom hook

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Switch, Route } from 'react-router-dom';
 import { Home, Todo, TodoSolo } from './pages'
 import { Layout } from './layout'
-import { useSelector, useDispatch } from 'react-redux'
-import { loadState } from "./actions";
+import { useLocalState } from './customHooks'
+
 
 const App = () => {
-    const dispatch = useDispatch()
 
-    const appState = useSelector((state) => state);
-
-    useEffect(() => {
-        const storeState = localStorage.getItem('state')
-        console.log(storeState.length)
-        if (storeState.length > 2) {
-        dispatch(loadState(JSON.parse(storeState)))
-        }
-        else {}
-    }, []);
-
-    useEffect(() => {
-        localStorage.setItem('state', JSON.stringify(appState))
-    }, [appState]);
+  useLocalState()
 
   return (
     <Layout>
