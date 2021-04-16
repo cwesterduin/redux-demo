8529a81b96f02db1322a8aa37e5d9c4f63f95275
chris westerduin <chriswesterduin@outlook.com>
2021-04-16 15:11:03 +0100

    add react-router in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
-// import { Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 const App = () => {
-    return (
-        <h1>Welcome</h1>
-    )
-}
+  return (
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/todo" component={About} />
+    </Switch>
+  );
+};
 
 export default App
