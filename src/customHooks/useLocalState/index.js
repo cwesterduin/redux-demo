e22bc452bb74366511ad755770ed753760620b20
chris westerduin <chriswesterduin@outlook.com>
2021-04-17 01:01:50 +0100

    remove console log

--- src/customHooks/useLocalState/index.js
+++ src/customHooks/useLocalState/index.js
@@ -1,24 +1,23 @@
-import { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux'
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { loadState } from "../../actions";
 
-function useLocalState() {  
-    const dispatch = useDispatch()
+function useLocalState() {
+  const dispatch = useDispatch();
 
-    const appState = useSelector((state) => state);
+  const appState = useSelector((state) => state);
 
-    useEffect(() => {
-        const storeState = localStorage.getItem('state')
-        console.log(storeState.length)
-        if (storeState.length > 2) {
-        dispatch(loadState(JSON.parse(storeState)))
-        }
-        else {}
-    }, []);
+  useEffect(() => {
+    const storeState = localStorage.getItem("state");
+    if (storeState.length > 2) {
+      dispatch(loadState(JSON.parse(storeState)));
+    } else {
+    }
+  }, []);
 
-    useEffect(() => {
-        localStorage.setItem('state', JSON.stringify(appState))
-    }, [appState]);
+  useEffect(() => {
+    localStorage.setItem("state", JSON.stringify(appState));
+  }, [appState]);
 }
 
-export default useLocalState
+export default useLocalState;
